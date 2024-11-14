19a3c31b29151b6310be55e6f6cc572dfc6256a2
Davidsb <121972361+Davidsb04@users.noreply.github.com>
2024-11-14 16:11:32 -0300

    Delete src/backend/services directory

--- src/backend/services/userServices.py
+++
@@ -1,8 +0,0 @@
-from data.firebaseSettings import db
-
-
-def is_unique_email(email):
-    email_exists = db.collection('users').where('email', '==', email).stream()
-    if any(email_exists):
-        return False
-    return True
